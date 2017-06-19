e38e1e250732d59bc194e0cd9bb5100e7b84ab8e
Johan Kladder <j.kladder@st.hanze.nl>
2017-06-19 08:56:25 +0200

    + Template params adding

--- src/Initializer/Template/ReleazTemplate.php
+++ src/Initializer/Template/ReleazTemplate.php
@@ -32,9 +32,37 @@ class ReleazTemplate extends FrameworkTemplate
      */
     public function initialize($filePath, $params)
     {
-        copy(__DIR__ . '/../../../recipe/releaz.php', $filePath);
-        copy(__DIR__ . '/../../../recipe/' . $this->getExample(),
-            dirname($filePath) . '/' . $this->getExample());
+        copy(__DIR__ . '/../../../recipe/releaz.php', $filePath); // Copy the deploy file.
+
+        $exampleFIle = $this->getExamplePath(); // Get the path of the example file.
+
+        $this->setParamsInExample($exampleFIle, $params); // Insert the params
+
+        copy($exampleFIle, dirname($filePath) . '/' . $this->getExample()); // Copy content.
+    }
+
+    private function getExamplePath()
+    {
+        return __DIR__ . '/../../../recipe/' . $this->getExample();
+    }
+
+    private function setParamsInExample($exampleFile, $params)
+    {
+        $content = file_get_contents($exampleFile);
+
+        $repoUrl = $this->getRepositoryFromParams($params);
+
+        $content = str_replace('[REPO_URL]', $repoUrl, $content);
+
+        file_put_contents($exampleFile, $content);
+    }
+
+    private function getRepositoryFromParams($params)
+    {
+        if (array_key_exists('repository', $params)) {
+            return $params['repository'];
+        }
+        return 'git@github.com:johankladder/releaz-deployer.git';
     }
 
 
